49b96bbc8f96f8dd7a1332ed66f15b809b3323ec
manish <code.manish94@gmail.com>
2020-02-11 21:58:53 +0530

    IdenticalTree

--- src/trees/binarytree/IdenticalTree.java
+++ src/trees/binarytree/IdenticalTree.java
@@ -12,4 +12,41 @@ public class IdenticalTree {
             this.right = null;
         }
     }
+
+    TreeNode root1;
+    TreeNode root2;
+
+    boolean identicalTrees(TreeNode node1, TreeNode node2){
+        if(null == node1 && null == node2){
+            return true;
+        }
+        if(null != node1 && null != node2){
+            return (node1.data == node2.data
+                    && identicalTrees(node1.left, node2.left)
+                    && identicalTrees(node1.right, node2.right)
+            );
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        IdenticalTree tree = new IdenticalTree();
+        tree.root1 = new TreeNode(1);
+        tree.root1.left = new TreeNode(2);
+        tree.root1.right = new TreeNode(3);
+        tree.root1.left.left = new TreeNode(4);
+        tree.root1.left.right = new TreeNode(5);
+
+        tree.root2 = new TreeNode(1);
+        tree.root2.left = new TreeNode(2);
+        tree.root2.right = new TreeNode(3);
+        tree.root2.left.left = new TreeNode(4);
+        tree.root2.left.right = new TreeNode(5);
+
+        if(tree.identicalTrees(tree.root1, tree.root2)){
+            System.out.println("Identical Tree");
+        } else {
+            System.out.println("Trees are not identical");
+        }
+    }
 }
