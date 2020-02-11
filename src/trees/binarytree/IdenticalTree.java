bdcdeaf6b4fdbd2ee550aa5af809b2ae0eb3614d
manish <code.manish94@gmail.com>
2020-02-11 08:49:15 +0530

    IdenticalTree

---
+++ src/trees/binarytree/IdenticalTree.java
@@ -0,0 +1,15 @@
+package trees.binarytree;
+
+public class IdenticalTree {
+    static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
